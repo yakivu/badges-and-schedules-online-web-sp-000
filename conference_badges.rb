2c3871279400a9bc1725a67768e86e72d0486b9b
Adam Walker <adamw207@yahoo.com>
2020-02-24 14:44:18 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+batch_badge_creator
+
+assign_rooms
+puts "Room Number #{   }"
+
+rooms = [1, 2, 3, 4, 5, 6, 7]
+
+printer
+
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+
+badge_maker("Arel")
+
+puts "Hello my name is #{     }"
